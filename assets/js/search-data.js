8611dfe642c1d0d0a76beca36e752941bcb3d077
lorenzo-carpentieri <lorenzocarpentieri34@gmail.com>
2025-03-19 17:06:22 +0000

    Deploying to gh-pages from @ lorenzo-carpentieri/lorenzo-carpentieri.github.io@1a3e725d2b5e8c407d34e3e970f7aeee7bc7776e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Hi, here you can find my CV. If you have any question please contact me :smile:.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
